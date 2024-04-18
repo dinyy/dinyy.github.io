fe29311db508091b7afe48c17206a27c3afe238f
dinyy <1076819326@qq.com>
2024-04-18 23:05:48 +0800

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://dinyy.github.io/",
   base: "/",
   integrations: [
     tailwind(),
---
+++ src/content/posts/test_blog.md
@@ -0,0 +1,9 @@
+---
+title: test_blog
+published: 2024-04-18
+description: ''
+image: ''
+tags: []
+category: ''
+draft: false 
+---
