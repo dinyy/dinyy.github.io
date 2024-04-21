b309f479fc3fd0f78c61f3f8c5eb285e964d75e4
dinyy <1076819326@qq.com>
2024-04-21 23:53:44 +0800

    修改细节

--- src/config.ts
+++ src/config.ts
@@ -32,23 +32,13 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'dinyy',
+  bio: 'Being dazzling.You are qualified.',
   links: [
-    {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',
-      url: 'https://twitter.com',
-    },
-    {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
-    },
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/dinyy',
     },
   ],
 }
